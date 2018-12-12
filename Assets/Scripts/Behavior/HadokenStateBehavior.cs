cb5de1e11e09ea2b2c6646523f6a298226bcbfab
Syed Khawar Ali Shah <khawarali5@gmail.com>
2018-12-12 14:48:02 +0500

    First commit

---
+++ Assets/Scripts/Behavior/HadokenStateBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HadokenStateBehavior : FighterStateBehavior {
+
+	string hadokenPath = "";
+	override public void OnStateEnter (Animator animator,
+		AnimatorStateInfo stateInfo, int layerIndex) {
+		base.OnStateEnter (animator, stateInfo, layerIndex);
+
+		float fighterX = fighter.transform.position.x;
+
+		if (fighter.player == Fighter.PlayerType.player1) {
+			Debug.Log ("hello");
+			hadokenPath = "Sfx/Hadoken";
+		} else {
+			Debug.Log ("hello 2");
+			hadokenPath = "Sfx/Hadoken_left";
+		}
+
+		GameObject instance = Object.Instantiate (
+			// Resources.Load("Sfx/Hadoken"),
+			Resources.Load (hadokenPath),
+			new Vector3 (fighterX, 1, 0),
+			Quaternion.Euler (0, 0, 0)
+		) as GameObject;
+
+		Hadoken hadoken = instance.GetComponent<Hadoken> ();
+		hadoken.caster = fighter;
+	}
+}
---
+++ Assets/Scripts/Entities/Hadoken.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Hadoken : MonoBehaviour {
+	public Fighter caster;
+	public Fighter fighter;
+	public float movementForce = 200;
+	public float damage;
+
+	private Rigidbody body;
+	private float creationTime;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hadok " + this.gameObject.name);
+		creationTime = Time.time;
+		body = GetComponent<Rigidbody> ();
+		// if (Fighter.player == Fighter.PlayerType.player1) {
+		// 	body.AddRelativeForce (new Vector3 (movementForce, 0, 0));
+		// } else {
+		// 	body.AddRelativeForce (new Vector3 (-movementForce, 0, 0));
+		// }
+		body.AddRelativeForce (new Vector3 (movementForce, 0, 0));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - creationTime > 3) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter (Collision col) {
+		Fighter fighter = col.gameObject.GetComponent<Fighter> ();
+		if (fighter != null && fighter != caster) {
+			fighter.hurt (damage);
+			Destroy (gameObject);
+		}
+	}
+}
